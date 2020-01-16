719e66954380e34f5add517e2e150fc43d73ea9c
Ronnie <oduor.ron@gmail.com>
2020-01-16 10:48:23 +0300

    local changes

---
+++ app/Dashboard.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Dashboard extends Model
+{
+    public $table = 'mlab_data';
+    public $timestamps = false;
+    public $incrementing = false;
+    
+    protected $fillable = [
+    ];
+}
