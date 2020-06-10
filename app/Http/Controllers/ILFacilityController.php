8d5a5206c7b354c4dad7da82843b296e3e3c32af
ronnie <oduor.ron@gmail.com>
2020-06-10 09:30:25 +0300

    added intert option selection while adding IL facility

--- app/Http/Controllers/ILFacilityController.php
+++ app/Http/Controllers/ILFacilityController.php
@@ -10,14 +10,15 @@ use App\Facility;
 
 class ILFacilityController extends Controller
 {
-    public function index(){
-
+    public function index()
+    {
         $facilities = ILFacility::with('facility.sub_county.county')->get();
 
         return view('facility.il_facility')->with('facilities', $facilities);
     }
 
-    public function addilfacilityform(){
+    public function addilfacilityform()
+    {
         $counties = County::all();
         $data = array(
             'counties' => $counties,
@@ -26,69 +27,71 @@ class ILFacilityController extends Controller
         return view('facility.addilfacility')->with($data);
     }
 
-    public function addilfacility(Request $request){
-        try{
+    public function addilfacility(Request $request)
+    {
+        try {
             $facility = new ILFacility;
 
             $facility->mfl_code = $request->code;
-            $facility->phone_no = $request->phone;
+            if (!empty($request->phone)) {
+                $facility->phone_no = $request->phone;
+            } else {
+                $facility->phone_no = $request->code;
+            }
+            $facility->internet = $request->internet;
 
-            if($facility->save()){
-
+            if ($facility->save()) {
                 toastr()->success('IL Facility has been saved successfully!');
 
                 return redirect()->route('il_facilities');
-            }else{
+            } else {
                 toastr()->error('An error has occurred please try again later.');
 
                 return back();
             }
-        }catch(Exception $e){
+        } catch (Exception $e) {
             toastr()->error('An error has occurred please try again later.');
 
             return back();
         }
-
     }
 
-    public function edit_ilfacility(Request $request){
-
-        try{
+    public function edit_ilfacility(Request $request)
+    {
+        try {
             $facility = ILFacility::where('mfl_code', $request->code)->first();
 
             $facility->phone_no = $request->phone;
             $facility->updated_at = date('Y-m-d H:i:s');
 
 
-            if($facility->save()){
-
+            if ($facility->save()) {
                 toastr()->success('IL Facility has been edited successfully!');
 
                 return redirect()->route('il_facilities');
-            }else{
+            } else {
                 toastr()->error('An error has occurred please try again later.');
 
                 return back();
             }
-        }catch(Exception $e){
+        } catch (Exception $e) {
             toastr()->error('An error has occurred please try again later.');
 
             return back();
         }
     }
 
-    public function delete_ilfacility(Request $request){
-
-        try{
+    public function delete_ilfacility(Request $request)
+    {
+        try {
             $facility = ILFacility::where('mfl_code', $request->code)->delete();
 
-            if($facility){
-
+            if ($facility) {
                 return response(['status' => 'success', 'details' => 'Facility has been deleted successfully']);
-            }else{
+            } else {
                 return response(['status' => 'error', 'details' => 'An error has occurred please try again later.']);
             }
-        }catch(Exception $e){
+        } catch (Exception $e) {
             return response(['status' => 'error', 'details' => 'An error has occurred please try again later.']);
         }
     }
